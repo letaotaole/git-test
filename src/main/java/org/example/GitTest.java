d992e5fd30d8b424b451def8107e7d06568e57b2
zy <Z58325Y@163.com>
2023-07-22 19:03:25 +0800

    first commit!

---
+++ src/main/java/org/example/GitTest.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git，hello git，hello git!");
+    }
+}
